cce143c262eb3acfc889d874ecbb1f8a9c3684a4
Link4Electronics <link4electronics@gmail.com>
2026-03-30 22:26:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/RigsOfRods/rigs-of-rods/refs/heads/master/doc/images/rorlogo.png
+export DESKTOP=DUMMY
+export MAIN_BIN=RoR
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./AppDir/bin/RoR
 
 # Additional changes can be done in between here
 
