acb0951de8d7f29d08426046711315a492f4a897
Link4Electronics <link4electronics@gmail.com>
2026-04-04 05:32:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,22 +6,16 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libxaw \
-    openal \
-    patchelf
+pacman -Syu --noconfirm libxaw openal npm pipewire-alsa pipewire-audio
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-make-aur-package itchio-downloader
-
-# If the application needs to be manually built that has to be done down here
 echo "Getting app..."
 echo "---------------------------------------------------------------"
 mkdir -p ./AppDir/bin
+npm install -g itchio-downloader@latest
 cd ./AppDir/bin
 itchio-downloader --url "https://rigs-of-rods.itch.io/rigs-of-rods" --platform linux --downloadDirectory .
 bsdtar -xvf ./game-2066563.zip
@@ -29,5 +23,7 @@ patchelf --set-rpath '$ORIGIN/lib' ./RoR
 patchelf --set-rpath '$ORIGIN' ./lib/*.so*
 rm -f ./*.zip ./RunRoR
 
-# TODO find a way to set version automatically
-echo '2026.01' > ~/version
+# TODO find a way to set version automatically, will leave this as reference in case dev doesn't sync with GH releases tag version
+#echo '2026.01' > ~/version
+VERSION="$(git ls-remote --tags --sort="v:refname" https://github.com/RigsOfRods/rigs-of-rods | grep -o '20[0-9]\{2\}\.[0-9]\{2\}' | tail -n1)"
+echo "$VERSION" > ~/version
